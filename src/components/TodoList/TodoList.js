0828ed1a4ec9acc46ca454c77b6b25903d85dd9f
cyberpirate92 <bsravi.tez@gmail.com>
2019-06-11 18:08:28 +0530

    Abiity to add item

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -8,7 +8,9 @@ class TodoList extends Component {
         this.state = {
             list: this.props.list,
         };
-        this.itemChangeHandler = this.itemChangeHandler.bind(this);
+        // this.itemChangeHandler = this.itemChangeHandler.bind(this);
+        // this.itemRemoveHandler = this.itemRemoveHandler.bind(this);
+        this.addTodoItem = this.addTodoItem.bind(this);
     }
 
     itemChangeHandler(itemIndex) {
@@ -27,15 +29,44 @@ class TodoList extends Component {
         curState.splice(itemIndex, 1);
         console.log('After', curState);
         this.setState({
-            list: curState
+            list: curState,
         });
+    }
+
+    addTodoItem(todoText) {
+        if (todoText) {
+            /** @type {Array} */
+            let curState = JSON.parse(JSON.stringify(this.state.list));
+            let maxId = (curState.length > 0 && curState.reduce((acc, cur, ) => cur.id > acc.id ? cur : acc).id) || 0;
+            curState.push({
+                done: false,
+                text: todoText,
+                id: maxId + 1,
+            });
+            this.setState({
+                list: curState,
+            });
+        }
+    }
+
+    addClickHandler = () => {
+        /** @type {HTMLInputElement} */
+        let textbox = document.querySelector("#todoInput");
+        if (textbox && textbox.value) {
+            let text = textbox.value.trim();
+            if (text) {
+                this.addTodoItem(text);
+                textbox.value = '';
+            }
+        }
     }
 
     render() {
         console.log('Rendering TodoList');
         return (
             <div className="TodoList">
-                <div>To-Do List {this.state.list.filter(item => item.done === true).length}/{this.state.list.length} Done</div>
+                <div>{this.state.list.filter(item => item.done === true).length}/{this.state.list.length} Done</div>
+                <input type="text" id="todoInput" placeholder="Add new item..."/> <button onClick={this.addClickHandler}>Add</button>
                 {this.state.list.map((item, index) => (<TodoItem key={item.id} done={item.done} text={item.text} changeHandler={() => this.itemChangeHandler(index)} removeHandler={() => this.itemRemoveHandler(index)} />))}
             </div>
         );
